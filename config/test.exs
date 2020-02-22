3ca778faea2314711fa66c34dce01c46718713d7
Raphael Barreto <rfsbarreto@gmail.com>
2020-02-22 16:33:32 -0300

    Adjust database config

--- config/test.exs
+++ config/test.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 # Configure your database
 config :backend_vagas, BackendVagas.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "backend_vagas_test",
+  username: System.get_env("POSTGRES_USER"),
+  password: System.get_env("POSTGRES_PSWD"),
+  database: System.get_env("POSTGRES_DATABASE"),
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
