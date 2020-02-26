228165be253ce42ea4d900c6863d515c448bb5b8
Raphael Barreto <rfsbarreto@gmail.com>
2020-02-26 10:50:26 -0300

    Remove conditional from test.exs and add env variables to Actions config file

--- config/test.exs
+++ config/test.exs
@@ -8,12 +8,6 @@ config :backend_vagas, BackendVagas.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
-if System.get_env("GITHUB_ACTIONS") do
-  config :backend_vagas, BackendVagas.Repo,
-    username: 'postgres',
-    password: 'postgres'
-end
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :backend_vagas, BackendVagasWeb.Endpoint,
